011d8c223a055061fd6a937a103870994754cec5
lahotikunal <37633212+lahotikunal@users.noreply.github.com>
2021-05-31 23:03:02 +0530

    Update index7java.js

--- index7java.js
+++ index7java.js
@@ -4,3 +4,8 @@ rules.onclick = function () {
       {location.href = "https://codepen.io/kunal28/pen/pobjEoX";}
   
 };
+
+demo.onclick = function () {
+      {location.href = "https://www.youtube.com/watch?v=OrnuYqP_XNM";}
+  
+};
